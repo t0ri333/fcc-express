806689eb2b3178d0431a8d4c4075f92d6c935fc2
tori <ziliangz@gmail.com>
2022-10-05 23:27:51 +0000

    add env variables to determine json object

--- myApp.js
+++ myApp.js
@@ -6,9 +6,12 @@ app.get('/', (req, res) => {
 });
 
 
+app.get('/json', (req,res)=>{
+  let msg = process.env.MESSAGE_STYLE === "uppercase"?"HELLO JSON" : "Hello json";
+  res.json({"message": msg});
+});
+
 app.use("/public", express.static(__dirname + "/public"));
-
-
 
 
 

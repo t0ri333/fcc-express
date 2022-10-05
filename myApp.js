bee8515e849409c7b47c7417627f8f514a3b42ae
tori <ziliangz@gmail.com>
2022-10-05 23:42:08 +0000

    add request logger middleware

--- myApp.js
+++ myApp.js
@@ -5,14 +5,17 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-app.get('/json', (req,res)=>{
+app.get('/json', (req,res) => {
   let msg = process.env.MESSAGE_STYLE === "uppercase"?"HELLO JSON" : "Hello json";
   res.json({"message": msg});
 });
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use(function middleware(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
 
 
 
